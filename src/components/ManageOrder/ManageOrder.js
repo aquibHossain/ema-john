6b36c37eb1c7517e8c228b695d7326b119d934a7
Aquib <hossainaquib20@gmail.com>
2022-01-26 23:54:09 +0600

    all

--- src/components/ManageOrder/ManageOrder.js
+++ src/components/ManageOrder/ManageOrder.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useHistory } from 'react-router';
+import { useState,useEffect } from 'react';
 import useAuthContext from '../Contexts/useAuthContext';
 
 const ManageOrder = () => {
     const [order,setOrder]=useState([])
+    const history =useHistory()
     const {user}=useAuthContext()
     useEffect(()=>{
-       fetch(`https://intense-dawn-27289.herokuapp.com/orders?email=${user.email}`)
-       .then(res=>res.json())
+       fetch(`https://intense-dawn-27289.herokuapp.com/orders?email=${user.email}`,{
+           headers:{
+               'authorization':`Bearer ${localStorage.getItem('idToken')}`
+           }
+       })
+       .then(res=>{
+           if(res.status==200){
+            res.json()
+           }
+           else if(res.status==401){
+               history.push('/login')
+           }
+       })
        .then(data=>setOrder(data))
     },[])
     return (
